9e3bec6ded7dfb6978c81968ed7ab99aaa721655
GiorgosSimos <giorgos_Simos@windowslive.com>
2023-10-20 00:17:48 +0300

    -Trying to create a lexicon  and a parser for the game

---
+++ nlp/parser.py
@@ -0,0 +1,34 @@
+from lexicon import Lexicon
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+from lexicon import Lexicon  # Import the Lexicon class
+
+# Create an instance of the Lexicon class
+lexicon_instance = Lexicon()
+
+Dune = """
+    Muad'Dib learned rapidly because his first training was in how to learn.
+    And the first lesson of all was the basic trust that he could learn.
+    It's shocking to find how many people do not believe they can learn,
+    and how many more believe learning to be difficult."""
+
+# Tokenize the text
+print(lexicon_instance.tokenize(Dune))
+
+worf_quote = "Sir, I protest. I am not a merry man!"
+words_in_quote = lexicon_instance.tokenize(worf_quote)
+
+print(words_in_quote)
+
+# Access the 'stops' dictionary from the Lexicon instance
+stop_words = lexicon_instance.vocab["stop"]
+
+filtered_list = []
+
+for word in words_in_quote:
+    if word[0] not in stop_words:
+        filtered_list.append(word)
+
+print(filtered_list)
+
